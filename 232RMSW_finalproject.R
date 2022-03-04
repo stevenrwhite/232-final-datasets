4b92e471b5aebc0a2ed2a01de16fbffcafc99eef
stevenrwhite <100866539+stevenrwhite@users.noreply.github.com>
2022-03-03 22:03:01 -0500

    Add files via upload

--- 232RMSW_finalproject.R
+++ 232RMSW_finalproject.R
@@ -1,54 +1,99 @@
-# [PREAMBLE]
-#
+# [START HERE]
 # [FILENAME] --> 232RMSW_finalproject.R
 #
 # [NAMES] --> Rachel Mroz, Steven White
-#
 # [DATE] --> 03/16/2022
-# 
-# [EXTRA PACKAGEES TO INSTALL NOT DISCUSSED IN CLASS] --> leaflet
+# [EXTRA PACKAGEES TO INSTALL NOT DISCUSSED IN CLASS] --> ggrepel, plotly
 # 
 # [DESCRIPTION] --> This code accomplishes the following tasks...
 # 
-# GRAPH 1: mil_spending_total (continuous) (ORDERED BAR GRAPH)
-# GRAPH 2: mil_spend_pct_gdp  (continuous) (BUBBLE MAP, BIGGEST BUBBLE==MOST SPENDING)
-# GRAPH 3: ruggedness, mil_spend_pct_gdp (both continuous) (SCATTER)
-# 
-# OTHER VARIABLES: country (list of countries)
+# GRAPH 1: [SCATTER PLOT VIEW IN VIEWER] variables: total_sch_adult, gdppc_ppp
+        # Creates an interactive scatter plot using plotly package
+        # comparing adult education rates to a country's GDP per capita.
+        # Runs regression line through data points.
+        # IN CODE: Prints correlation between the two variables.
+        # INTERACTIVE: Use mouse to hover over points and regression line
+        # to reveal information about each point, including exact point coordinates
+        # and country name.
+#
+# GRAPH 2: [HORIZONTAL BAR GRAPH VIEW IN PLOTS] variables: country, mil_spend+total
+        # Creates bar graph of military spending, ordered from highest to lowest.
+        # Filters results to show only top *****INSERT NUMBER***** highest spending countries.
+        # Rotates bar graph from initial position to for ease of viewing,
+        # in addition to other aesthetic choices.
+        #
+        # *****ADD MORE WHEN DONE!!*****
+        #
+#
+# GRAPH 3: [INTERACTIVE BUBBLE PLOT VIEW IN VIEWER] variables: basic_water_access, childmort_
+        # Creates an interactive bubble plot (based on country population) 
+        # comparing a country's access to water and its child mortality rate.
+        # Rruns regression line through data points.
+        # IN CODE: Prints correlation between the two variables.
+        # INTERACTIVE: Use mouse to hover over points and regression line
+        # to reveal information about each specific point, including exact point coordinates
+        # and country name.
 #
 #
 #
 # Load packages
 library(tidyverse)
-library(leaflet)
+library(ggrepel)
+library(plotly)
 
 # Read data
 cntry_stats <- read_csv('https://raw.githubusercontent.com/ameliahg/PSCI232/main/country_stats_recent.csv')
+
 
 
 # Creating regression m1
 m1 <- lm(total_sch_adult~gdppc_ppp, data=cntry_stats)
 print(summary(m1))
 # Creating scatter plot s1
-s1 <- ggplot(data=cntry_stats, aes(x=total_sch_adult,
-                           y=gdppc_ppp)) +
-  geom_point(alpha=0.4, color="red") + geom_smooth(method="lm")
-print(s1, na.rm=TRUE)
+s1 <- ggplot(data=cntry_stats, aes(x=total_sch_adult, y=gdppc_ppp, 
+                                   group=1,
+                                   text= paste("Country: ", country))) +
+  theme_bw()+
+  geom_point(alpha=0.6, color="darkgreen") + geom_smooth(method="lm")
+#ggplotly(s1) --> IF highlighted and run, will open in viewer to become interactive.
+
+print(ggplotly(s1 +  labs(x="Education Level", y="GDP per capita",
+                          title="Education Rates Compared to Country GDP")),
+      na.rm=TRUE) # Running ggplotly in this line opens the graph in viewer tab.
+                  # Does not make .pdf file interactive.
+print(cor(cntry_stats$total_sch_adult,cntry_stats$gdppc_ppp,use='complete.obs'))
 ggsave('s1.pdf',plot=s1,width=6,height=6)
 
 
-#Creating bar grapg b1
-b1 <- ggplot(data=cntry_stats, aes(x=reorder(country, -mil_spend_total),
+
+# Creating bar graph b1
+b1 <- ggplot(data=cntry_stats, aes(x=reorder(country, +mil_spend_total),
                                    y=sqrt(mil_spend_total)))+
-  geom_col()
-print(b1 +labs(x="country", y="Spending (USD)",
+  geom_col(color="white", fill="darkred")
+print(b1 + coord_flip() +
+        labs(x="country", y="Spending (USD)",
                                title="Military Spending Total By Country",
-                               subtitle="From highest to lowest"), na.rm=TRUE)
+                               subtitle="From highest to lowest"),
+      na.rm=TRUE)
 ggsave('b1.pdf', plot=b1,width=10,height=10)
 
 
 
-
+# Regression for bub1
+m2 <- lm(basic_water_access~childmort_, data=cntry_stats)
+print(summary(m2))
+# Creating bubble plot
+bub1 <- ggplot(data=cntry_stats, aes(x=basic_water_access, y=childmort_, size=pop_total,
+                                     group=1,
+                                     text= paste("Country: ", country))) +
+  theme_bw()+
+  geom_point(alpha=0.2, color="blue") + geom_smooth(method="lm")
+print(ggplotly(bub1 +  labs(x="Basic Water Access", y="Child Mortality Rate",
+                          title="Correlation of Access to Water and Child Mortality By Country")),
+      na.rm=TRUE)
+# Print Correlation --> cor==-0.8348726
+print(cor(cntry_stats$basic_water_access,cntry_stats$childmort_,use='complete.obs'))
+ggsave('bub1.pdf',plot=bub1,width=6,height=6)
 
 
 
